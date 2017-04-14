cb86ca4f8eb5af91798249c7c04171b6ac38ab78
Adrian Hecker <starcalibre@gmail.com>
2017-04-14 11:28:53 +1000

    Add unit test for NavigationItemViewModel

---
+++ Starter/FriendStorage/FriendStorage.UITests/ViewModel/NavigationItemViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using FriendStorage.UI.Messages;
+using FriendStorage.UI.ViewModel;
+using GalaSoft.MvvmLight.Messaging;
+using Moq;
+using NUnit.Framework;
+
+namespace FriendStorage.UITests.ViewModel
+{
+    [TestFixture]
+    public class NavigationItemViewModelTest
+    {
+        [Test]
+        public void ShouldPublishOpenFriendEditViewEvent()
+        {
+            const int friendId = 7;
+
+            var messengerMock = new Mock<IMessenger>();
+            var viewModel = new NavigationItemViewModel(friendId, "Thomas", messengerMock.Object);
+            var message = new OpenFriendEditViewMessage(friendId);
+
+            viewModel.OpenFriendEditViewCommand.Execute(null);
+
+            messengerMock.Verify(m => m.Send(message), Times.Once);
+        }
+    }
+}
