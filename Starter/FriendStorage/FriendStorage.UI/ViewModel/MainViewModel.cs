b81e8ee726e5582c99cae0dc7bb05bdc4523ad22
Adrian Hecker <starcalibre@gmail.com>
2017-04-14 12:41:14 +1000

    Add FriendEditViewModel to MainViewModel

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -1,12 +1,41 @@
-﻿namespace FriendStorage.UI.ViewModel
+﻿using System;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace FriendStorage.UI.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Private Fields
+
+        private FriendEditViewModel _selectedFriendEditViewModel;
+        private Func<IFriendEditViewModel> _friendEditVmCreator;
+        private IMessenger _messenger;
+
+        #endregion
+
+        #region Properties
+
+        public INavigationViewModel NavigationViewModel { get; set; }
+        public ObservableCollection<IFriendEditViewModel> FriendEditViewModels { get; private set; }
+
+        public FriendEditViewModel SelectedFriendEditViewModel
+        {
+            get { return _selectedFriendEditViewModel; }
+            set { _selectedFriendEditViewModel = value; }
+        }
+
+        #endregion
+
         #region Constructor
 
-        public MainViewModel(INavigationViewModel navigationViewModel)
+        public MainViewModel(INavigationViewModel navigationViewModel, 
+            Func<IFriendEditViewModel> friendEditVmCreator, IMessenger messenger)
         {
             NavigationViewModel = navigationViewModel;
+            FriendEditViewModels = new ObservableCollection<IFriendEditViewModel>();
+            _friendEditVmCreator = friendEditVmCreator;
+            _messenger = messenger;
         }
 
         #endregion
@@ -17,12 +46,6 @@
         {
             NavigationViewModel.Load();
         }
-
-        #endregion
-
-        #region Properties
-
-        public INavigationViewModel NavigationViewModel { get; set; }
 
         #endregion
     }
