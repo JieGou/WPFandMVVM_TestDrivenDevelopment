b200e4fda889ec19f440396ac488859eba060805
Adrian Hecker <starcalibre@gmail.com>
2017-04-14 11:28:24 +1000

    Add OpenFriendEditViewMessage class

---
+++ Starter/FriendStorage/FriendStorage.UI/Messages/OpenFriendEditViewMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace FriendStorage.UI.Messages
+{
+    public class OpenFriendEditViewMessage
+    {
+        #region Properties
+
+        public int FriendId { get; private set; }
+        
+        #endregion
+
+        #region Constructor
+
+        public OpenFriendEditViewMessage(int friendId)
+        {
+            FriendId = friendId;
+        }
+
+        #endregion
+    }
+}
