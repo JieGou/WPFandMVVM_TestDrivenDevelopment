0cf8fa6642e2b0e68d8490b01de6690567df3fd2
Adrian Hecker <starcalibre@gmail.com>
2017-04-17 17:52:32 +1000

    Add unit test for selecting/loading friends in MainViewModel

--- Starter/FriendStorage/FriendStorage.UITests/ViewModel/MainViewModelTests.cs
+++ Starter/FriendStorage/FriendStorage.UITests/ViewModel/MainViewModelTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using FriendStorage.UI.Messages;
 using FriendStorage.UI.ViewModel;
 using GalaSoft.MvvmLight.Messaging;
 using Moq;
@@ -11,7 +13,7 @@ namespace FriendStorage.UITests.ViewModel
     {
         private Mock<INavigationViewModel> _navigationVmMock;
         private MainViewModel _viewModel;
-        private Mock<IMessenger> _mockMessenger;
+        private IMessenger _testMessenger;
         private List<Mock<IFriendEditViewModel>> _friendEditViewModelMocks;
 
         private readonly MockRepository _mockRepository = new MockRepository(MockBehavior.Loose);
@@ -21,10 +23,10 @@ namespace FriendStorage.UITests.ViewModel
         {
             _navigationVmMock = new Mock<INavigationViewModel>();
             _friendEditViewModelMocks = new List<Mock<IFriendEditViewModel>>();
-            _mockMessenger = _mockRepository.Create<IMessenger>();
+            _testMessenger = new Messenger();
 
             _viewModel = new MainViewModel(_navigationVmMock.Object, CreateFriendEditViewModel,
-                _mockMessenger.Object);
+                _testMessenger);
         }
 
         [Test]
@@ -32,6 +34,20 @@ namespace FriendStorage.UITests.ViewModel
         {
             _viewModel.Load();
             _navigationVmMock.Verify(vm => vm.Load(), Times.Once);
+        }
+
+        [Test]
+        public void ShouldAddFriendEditViewModelAndLoadAndSelectIt()
+        {
+            const int friendId = 7;
+            _testMessenger.Send(new OpenFriendEditViewMessage(friendId));
+
+            Assert.AreEqual(1, _viewModel.FriendEditViewModels.Count);
+
+            var friendEditVm = _viewModel.FriendEditViewModels.First();
+            Assert.AreEqual(friendEditVm, _viewModel.SelectedFriendEditViewModel);
+
+            _friendEditViewModelMocks.First().Verify(vm => vm.Load(friendId), Times.Once);
         }
 
         #region Private Methods
