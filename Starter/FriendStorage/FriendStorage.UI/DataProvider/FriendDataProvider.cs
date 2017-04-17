0bc90f693ed358060135552b47e9bfb84db8b3c7
Adrian Hecker <starcalibre@gmail.com>
2017-04-17 20:06:58 +1000

    Add IFriendDataProvider interface and FriendDataProvider class

---
+++ Starter/FriendStorage/FriendStorage.UI/DataProvider/FriendDataProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Markup;
+using FriendStorage.DataAccess;
+using FriendStorage.Model;
+
+namespace FriendStorage.UI.DataProvider
+{
+    public class FriendDataProvider : IFriendDataProvider
+    {
+        #region Private Fields
+
+        private readonly Func<IDataService> _dataServiceCreator;
+        
+        #endregion
+
+        #region Constructor
+
+        public FriendDataProvider(Func<IDataService> dataServiceCreator)
+        {
+            _dataServiceCreator = dataServiceCreator;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public Friend GetFriendById(int id)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                return dataService.GetFriendById(id);
+            }
+        }
+
+        public void SaveFriend(Friend friend)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                dataService.SaveFriend(friend);
+            }
+        }
+
+        public void DeleteFriend(int id)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                dataService.DeleteFriend(id);
+            }
+        }
+
+        #endregion
+    }
+}
