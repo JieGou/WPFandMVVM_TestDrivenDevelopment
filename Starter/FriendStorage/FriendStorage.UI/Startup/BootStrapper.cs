197d6fd54d674f54dd8f9d53b8c3f8d8bda613ac
Adrian Hecker <starcalibre@gmail.com>
2017-04-17 20:57:09 +1000

    Add all new depencies to Autofac bootstrapper

--- Starter/FriendStorage/FriendStorage.UI/Startup/BootStrapper.cs
+++ Starter/FriendStorage/FriendStorage.UI/Startup/BootStrapper.cs
@@ -3,6 +3,7 @@ using FriendStorage.DataAccess;
 using FriendStorage.UI.DataProvider;
 using FriendStorage.UI.View;
 using FriendStorage.UI.ViewModel;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace FriendStorage.UI.Startup
 {
@@ -18,6 +19,9 @@ namespace FriendStorage.UI.Startup
             builder.RegisterType<NavigationViewModel>().As<INavigationViewModel>();
             builder.RegisterType<NavigationDataProvider>().As<INavigationDataProvider>();
             builder.RegisterType<FileDataService>().As<IDataService>();
+            builder.RegisterType<Messenger>().As<IMessenger>().SingleInstance();
+            builder.RegisterType<FriendDataProvider>().As<IFriendDataProvider>();
+            builder.RegisterType<FriendEditViewModel>().As<IFriendEditViewModel>();
 
             return builder.Build();
         }
