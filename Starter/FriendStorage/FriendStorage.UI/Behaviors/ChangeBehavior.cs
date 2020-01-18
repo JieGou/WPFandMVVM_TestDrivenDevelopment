37bb4ac0970b62d8ccfc6c8ea94b997fd3659896
Jan Pluskal <jan.pluskal@gmail.com>
2020-01-18 17:54:09 +0100

    Use IsActiveProperty to create dynamic bindings of IsChanged and OriginalValue for TextBox.

--- Starter/FriendStorage/FriendStorage.UI/Behaviors/ChangeBehavior.cs
+++ Starter/FriendStorage/FriendStorage.UI/Behaviors/ChangeBehavior.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace FriendStorage.UI.Behaviors
 {
@@ -10,6 +14,24 @@ namespace FriendStorage.UI.Behaviors
 
         public static readonly DependencyProperty OriginalValueProperty = DependencyProperty.RegisterAttached(
             "OriginalValue", typeof(object), typeof(ChangeBehavior), new PropertyMetadata(default(object)));
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.RegisterAttached(
+            "IsActive", typeof(bool), typeof(ChangeBehavior), new PropertyMetadata(default(bool), OnIsActivePropertyChanged));
+
+        private static readonly Dictionary<Type,DependencyProperty> _defaultProperties = new Dictionary<Type, DependencyProperty>()
+        {
+            [typeof(TextBox)] = TextBox.TextProperty
+        };  
+
+        public static void SetIsActive(DependencyObject element, bool value)
+        {
+            element.SetValue(IsActiveProperty, value);
+        }
+
+        public static bool GetIsActive(DependencyObject element)
+        {
+            return (bool) element.GetValue(IsActiveProperty);
+        }
 
         public static void SetIsChanged(DependencyObject element, bool value)
         {
@@ -29,6 +51,29 @@ namespace FriendStorage.UI.Behaviors
         public static object GetOriginalValue(DependencyObject element)
         {
             return element.GetValue(OriginalValueProperty);
+        }
+
+        private static void OnIsActivePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (_defaultProperties.ContainsKey(d.GetType()))
+            {
+                var defaultProperty = _defaultProperties[d.GetType()];
+                if ((bool) e.NewValue)
+                {
+                    var binding = BindingOperations.GetBinding(d, defaultProperty);
+                    if (binding != null)
+                    {
+                        var bindingPath = binding.Path.Path;
+                        BindingOperations.SetBinding(d, IsChangedProperty, new Binding(bindingPath + "IsChanged"));
+                        BindingOperations.SetBinding(d, OriginalValueProperty, new Binding(bindingPath + "OriginalValue"));
+                    }
+                }
+                else
+                {
+                    BindingOperations.ClearBinding(d,IsChangedProperty);
+                    BindingOperations.ClearBinding(d,OriginalValueProperty);
+                }
+            }
         }
     }
 }
