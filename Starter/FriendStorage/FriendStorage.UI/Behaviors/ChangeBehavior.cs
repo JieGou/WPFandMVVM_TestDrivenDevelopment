b2a0b0e69cd3a8ce2feeece16eefcf31d24e40da
Jan Pluskal <jan.pluskal@gmail.com>
2020-01-18 17:40:55 +0100

    Refactor change tracking reaction to be applicable on any textbox.

---
+++ Starter/FriendStorage/FriendStorage.UI/Behaviors/ChangeBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace FriendStorage.UI.Behaviors
+{
+    //Note generate code snippets in VS as `propa`, with Resharper as `attachedProperty`
+    public static class ChangeBehavior
+    {
+        public static readonly DependencyProperty IsChangedProperty = DependencyProperty.RegisterAttached(
+            "IsChanged", typeof(bool), typeof(ChangeBehavior), new PropertyMetadata(default(bool)));
+
+        public static readonly DependencyProperty OriginalValueProperty = DependencyProperty.RegisterAttached(
+            "OriginalValue", typeof(object), typeof(ChangeBehavior), new PropertyMetadata(default(object)));
+
+        public static void SetIsChanged(DependencyObject element, bool value)
+        {
+            element.SetValue(IsChangedProperty, value);
+        }
+
+        public static bool GetIsChanged(DependencyObject element)
+        {
+            return (bool) element.GetValue(IsChangedProperty);
+        }
+
+        public static void SetOriginalValue(DependencyObject element, object value)
+        {
+            element.SetValue(OriginalValueProperty, value);
+        }
+
+        public static object GetOriginalValue(DependencyObject element)
+        {
+            return element.GetValue(OriginalValueProperty);
+        }
+    }
+}
