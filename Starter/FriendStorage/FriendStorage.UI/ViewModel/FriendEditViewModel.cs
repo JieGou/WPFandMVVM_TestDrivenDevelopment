af1b66adfc2e04c5649b024be6e14a7aa9ed6866
Jan Pluskal <jan.pluskal@gmail.com>
2020-01-19 10:57:50 +0100

    Implement SaveCommand CanExecute on FriendEditViewModel to not allow the save of invalid model.

--- Starter/FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
+++ Starter/FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
@@ -87,7 +87,8 @@ namespace FriendStorage.UI.ViewModel
             Friend = new FriendWrapper(friend);
             Friend.PropertyChanged += (s, e) =>
             {
-                if (e.PropertyName == nameof(Friend.IsChanged)) InvalidateCommands();
+                if (e.PropertyName == nameof(Friend.IsChanged) 
+                    || e.PropertyName == nameof(Friend.IsValid)) InvalidateCommands();
             };
 
             InvalidateCommands();
@@ -113,7 +114,7 @@ namespace FriendStorage.UI.ViewModel
 
         private bool OnSaveCanExecute(object arg)
         {
-            return Friend.IsChanged;
+            return Friend.IsChanged && Friend.IsValid;
         }
 
         private void OnResetExecute(object obj)
